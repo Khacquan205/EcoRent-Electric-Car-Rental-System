842c5e538ed5f862c0aae42ade1198675e72dfea
DESKTOP-SVQM6UM\nhutl <nhutlmao251@gmail.com>
2026-01-27 13:41:44 +0700

    Add Dto

---
+++ backend/CAR.Application/Dtos/UserDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAR.Application.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+
+        public int RoleId { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public string? Phone { get; set; }
+
+        public short Gender { get; set; }
+
+        public short Status { get; set; }
+
+        public string? AvatarImgUrl { get; set; }
+    }
+}
