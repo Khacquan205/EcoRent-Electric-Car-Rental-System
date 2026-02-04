da3800f6940fe8f1529ef683f3c28e6a70eacc07
Nhwt <nhutlmao251@gmail.com>
2026-02-05 05:59:16 +0700

    Fix DependencyInjection conflicts - include all services

--- backend/CAR.Infrastructure/DependencyInjection.cs
+++ backend/CAR.Infrastructure/DependencyInjection.cs
@@ -36,22 +36,16 @@ namespace CAR.Infrastructure
             services.AddScoped<IPostRepository, PostRepository>();
             services.AddScoped<IOwnerPackageRepository, OwnerPackageRepository>();
             services.AddScoped<IRepository<Domain.Entities.MUser>, Repository<Domain.Entities.MUser>>();
-<<<<<<< HEAD
             services.AddScoped<IKycRepository, KycRepository>();
             services.AddScoped<IPhoneRepository, PhoneRepository>();
-=======
->>>>>>> e48b1cfac175a5e2424f3e1473341d311e08a3c6
 
             // Services
             services.AddScoped<IEmailService, EmailService>();
             services.AddScoped<IJwtService, JwtService>();
             services.AddScoped<IFirebaseService, FirebaseService>();
-<<<<<<< HEAD
             services.AddScoped<IKycOcrService, FptKycOcrService>();
             services.AddScoped<ITwilioSmsService, TwilioSmsService>();
             services.AddScoped<ICustomerService, CustomerService>();
-=======
->>>>>>> e48b1cfac175a5e2424f3e1473341d311e08a3c6
 
             return services;
         }
@@ -59,25 +53,17 @@ namespace CAR.Infrastructure
         public static IServiceCollection AddApplication(
             this IServiceCollection services)
         {
-<<<<<<< HEAD
-            // Application Services
-=======
             // Services
->>>>>>> e48b1cfac175a5e2424f3e1473341d311e08a3c6
             services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<IPostService, PostService>();
             services.AddScoped<IOwnerService, OwnerService>();
             services.AddScoped<ISubscriptionService, SubscriptionService>();
             services.AddScoped<IIdentityVerificationService, IdentityVerificationService>();
-<<<<<<< HEAD
             services.AddScoped<IKycOcrService, FptKycOcrService>();
             services.AddScoped<IFirebasePhoneService, FirebasePhoneService>();
             services.AddScoped<ICustomerKycService, CustomerKycService>();
- 
-=======
             services.AddScoped<IOwnerPackageService, OwnerPackageService>();
 
->>>>>>> e48b1cfac175a5e2424f3e1473341d311e08a3c6
             return services;
         }
     }
